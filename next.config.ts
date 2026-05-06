62bdab75fb9ba83b7a88b2d3e2d6533a3e69d4ec
dedybayu <dedybayusetiawan5@gmail.com>
2026-05-06 16:32:27 +0700

    fix: error build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['novnc-next'],
+  transpilePackages: ['novnc-next', '@maxmind/geoip2-node'],
 };
 
 export default nextConfig;
